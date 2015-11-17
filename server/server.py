7e1f8d5e88409d5be105efa8cb254d9e20c2106d
Richard Artoul <richardartoul@gmail.com>
2015-11-17 15:38:32 -0800

    [Feature] Retrieve access token

--- server/server.py
+++ server/server.py
@@ -1,9 +1,10 @@
 import tornado.ioloop
 import tornado.web
+import urllib
 from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httpclient import HTTPClient
-fro import config
+from config import config
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
@@ -14,20 +15,37 @@ class MainHandler(tornado.web.RequestHandler):
     self.write(response.body)    
 
 class AuthHandler(tornado.web.RequestHandler):
+  @gen.coroutine
   def get(self):
-    print(self.get_argument("code"))
+    auth_code = self.get_argument("code")
+    request_data = urllib.urlencode({
+      "client_secret": config["SECRET"],
+      "client_id": config["CLIENT_ID"],
+      "grant_type": "authorization_code",
+      "redirect_uri": config["REDIRECT_URI"],
+      "code": auth_code
+    })
+    print(request_data)
+    # Don't need to yield because we can fire and forget
+    response = yield AsyncHTTPClient().fetch(config["TOKEN_URI"], method="POST", body=request_data)
+    print(response.body)
+
+class TokenHandler(tornado.web.RequestHandler):
+  def get(self):
+    print("Token route called")
+
 
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
     (r"/login", tornado.web.RedirectHandler, dict(url=config["AUTH_REDIRECT"])),
-    (r"/auth", AuthHandler)
+    (r"/auth", AuthHandler),
+    (r"/token", TokenHandler)
   ])
 
 if __name__ == "__main__":
   app = make_app()
   app.listen(8888)
-  print(config["AUTH_REDIRECT"])
   print("Server listening on 8888")
   tornado.ioloop.IOLoop.current().start()
 
