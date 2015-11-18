3e2d759462ae73b046f40bd8449b591e6fa5bee0
Richard Artoul <richardartoul@gmail.com>
2015-11-18 02:29:35 -0800

    [Feature] Authhandler complete

--- server/auth_handler.py
+++ server/auth_handler.py
@@ -3,7 +3,21 @@ from tornado.httpclient import AsyncHTTPClient
 import tornado.web
 from tornado import gen
 import urllib
+import json
+from redis_conn import r
 
+class AuthHandler(tornado.web.RequestHandler):
+  @gen.coroutine
+  def get(self):
+    # Get tokens
+    token_data = yield request_token(self.get_argument("code"))
+    # Get user profile information
+    user_data = yield get_user(token_data["access_token"])
+    # Store user profile information and tokens
+    store_user(user_data, token_data)
+    # TODO: Send client JWT for further requests
+
+# Requests access / refresh token
 @gen.coroutine
 def request_token(auth_code):
   request_data = urllib.urlencode({
@@ -14,10 +28,26 @@ def request_token(auth_code):
     "code": auth_code
   })
   token_response = yield AsyncHTTPClient().fetch(config["TOKEN_URI"], method="POST", body=request_data)
-  raise gen.Return(token_response.body)
-  
-class AuthHandler(tornado.web.RequestHandler):
-  @gen.coroutine
-  def get(self):
-    token_data = yield request_token(self.get_argument("code"))
-    print(token_data)
+  raise gen.Return(json.loads(token_response.body))
+
+# Gets user profile information
+@gen.coroutine
+def get_user(access_token):
+  auth_header = {"Authorization": "BEARER " + access_token}
+  user_response = yield AsyncHTTPClient().fetch(
+    config["endpoints"]["profile"], 
+    method="GET", 
+    headers=auth_header)
+  raise gen.Return(json.loads(user_response.body))
+
+# Store user
+def store_user(user_data, token_data):
+  r.set(user_data["uuid"], {
+    "first_name": user_data["first_name"],
+    "last_name": user_data["last_name"],
+    "email": user_data["email"],
+    "tokens": {
+      "access": token_data["access_token"],
+      "refresh": token_data["refresh_token"]
+    }
+  })
