9e0ac5cd749cd6b3c1599cfb726634893b1fa90b
Richard Artoul <richardartoul@gmail.com>
2015-11-19 15:48:54 -0800

    [Feature] Request ride proxy

--- client/popup.js
+++ client/popup.js
@@ -1,13 +1,14 @@
 var app = {
   server: "http://localhost:8888/api/",
-  location: null
+  currentLocation: null,
+  selectedDestination: null,
 }
 
 // TODO: Make products load in background thread
 app.initialize = function() {
   if (this.isAuthenticated) {
     this.getLocation(function() {
-      if (this.location) {
+      if (this.currentLocation) {
         this.getProducts();
         this.loadAutoComplete();
       } else {
@@ -23,35 +24,60 @@ app.initialize = function() {
 app.getLocation = function(callback) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      this.location = {
+      this.currentLocation = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       }
       callback();
     }.bind(this));
   } else {
-    this.location = null;
+    this.currentLocation = null;
     callback();
   }
 };
 
 app.displayProducts = function(products) {
   products.forEach(function(product) {
-    var newRowString = "<tr><td>" +
-      product.display_name + "</td><td>" + 
-      product.description + "</td><td>" + 
+    var newRowString = '<tr class="product-row"><td>' +
+      product.display_name + '</td><td>' + 
+      product.description + '</td><td>' + 
       // Display N/A if no price details
-      (product.price_details ? product.price_details.cost_per_distance : "N/A") + "</td></tr>";
-    var $newRow = $(newRowString).attr("data-product-id", product.product_id);
+      (product.price_details ? product.price_details.cost_per_distance : 'N/A') + '</td></tr>';
+    var $newRow = $(newRowString).attr('data-product-id', product.product_id);
     $('#products-table').append($newRow);
   });
 
   // Remove loading bar
   $('#products-loader').removeClass("progress");
+
+  // Add click handler for requesting a ride
+  $('.product-row').click(function(event) {
+    var requestDetails = {
+      product_id: $(event.currentTarget).data("productId"),
+      start_latitude: this.currentLocation.latitude,
+      start_longitude: this.currentLocation.longitude
+    }
+
+    // Set destination only if user selected one already
+    if (this.selectedDestination) {
+      requestDetails.end_latitude = this.selectedDestination.latitude,
+      requestDetails.end_longitude = this.selectedDestination.longitude
+    }
+
+    $.ajax({
+      url: this.server + "requests",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(requestDetails)
+    })
+      .done(function(response) {
+        console.log(response);
+      }.bind(this));
+  }.bind(this));
 }
 
 app.getProducts = function() {
-  $.get(this.server + "products", this.location)
+  $.get(this.server + "products", this.currentLocation)
     .done(function(response) {
       this.displayProducts(response.products);
     }.bind(this));
@@ -61,15 +87,11 @@ app.loadAutoComplete = function() {
   autocompleteService = new google.maps.places.Autocomplete($('#search-places').get(0));
   autocompleteService.addListener('place_changed', function() {
     var place = autocompleteService.getPlace();
-    var selectedLocation = {
+    this.selectedDestination = {
       latitude: place.geometry.location.lat(),
       longitude: place.geometry.location.lng()
     }
-    // var requestRideData = {
-    //   product
-    // }
-    // $.post(server + "requests")
-  });
+  }.bind(this));
 };
 
 app.isAuthenticated = function(callback) {
--- server/ride_handler.py
+++ server/ride_handler.py
@@ -8,24 +8,34 @@ from config import config
 @gen.coroutine
 def request_ride(access_token, request_details):
   request_body = {
-    product_id: request_details["product_id"],
-    start_latitude: request_details["start_latitude"],
-    start_longitude: request_details["start_longitude"],
+    "product_id": request_details["product_id"],
+    "start_latitude": request_details["start_latitude"],
+    "start_longitude": request_details["start_longitude"],
   }
 
   if "end_latitude" in request_details and "end_longitude" in request_details:
     request_body["end_latitude"] = request_details["end_latitude"]
     request_body["end_longitude"] = request_details["end_longitude"]
 
-  request_body = urllib.urlencode(request_body)
+  request_body = json.dumps(request_body)
+  request_header = {
+    "Authorization": "BEARER " + access_token,
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+  }
 
-  request_response = yield AsyncHTTPClient().fetch(config["endpoints"]["requests"], method="POST", body=request_body)
-  yield json.loads(request_response.body)
+  print(request_body);
+  request_response = yield AsyncHTTPClient().fetch(
+    config["endpoints"]["requests"], 
+    method="POST", 
+    body=request_body,
+    headers=request_header)
+  raise gen.Return(json.loads(request_response.body))
 
 class RideHandler(BaseHandler):
   @gen.coroutine
   def post(self):
     request_details = json.loads(self.request.body)
-    request_data = request_ride(self.current_user["tokens"]["access"], request_details)
+    request_data = yield request_ride(self.current_user["tokens"]["access"], request_details)
     print(request_data)
     
