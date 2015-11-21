1a5974a06b55c415dc3e729f358d0c5781f610b3
Richard Artoul <richardartoul@gmail.com>
2015-11-21 15:51:09 -0800

    [Feature] Server websocket cleanup on close

--- server/request_status_handler.py
+++ server/request_status_handler.py
@@ -12,16 +12,19 @@ class RequestStatusHandler(tornado.websocket.WebSocketHandler):
     return True
 
   def open(self):
+    self.user_UUID = None
     print("Websocket connection established!")
-    pass
 
   def on_message(self, message):
     message = json.loads(message)
+    # Store connection in dictionary with UUID as key so messages can be sent later
     if (message["type"] == "auth"):
       decoded_user_jwt = jwt.decode(message["message"], config["JWT_SECRET"], algorithms=["HS256"])
-      # print(decoded_user_jwt)
       self.socket_connections[decoded_user_jwt["uuid"]] = self
+      # Also store UUID on instance so it can be removed from dictionary when connection closes
+      self.user_UUID = decoded_user_jwt["uuid"]
 
   def on_close(self):
+    # Remove key if exists, otherwise return default (None) -- Won't raise key-error
+    self.socket_connections.pop(self.user_UUID, None)
     print("Websocket connection closed!")
-    # pass
