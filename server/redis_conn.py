1cb35f0c85c5a00e4fe5f4289aec8df760d9a406
Richard Artoul <richardartoul@gmail.com>
2015-11-22 17:36:29 -0800

    [Deployment] Fixed typo in environment variable name

--- server/redis_conn.py
+++ server/redis_conn.py
@@ -2,7 +2,7 @@ import os
 import redis
 import urlparse
 if os.environ.get('REDIS_URL', 'localhost') != 'localhost':
-  url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+  url = urlparse.urlparse(os.environ.get('REDIS_URL'))
   r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 else:
-  r = redis.StrictRedis(host='localhost', port=6379, db=0)
+  r = redis.StrictRedis(host='localhost', port=6379h, db=0)
